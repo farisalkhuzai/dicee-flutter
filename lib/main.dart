d20d76ae28930d974fa80ca3f93817f255bd4063
farisalkhuzai <alkhuzaifaris@gmail.com>
2022-02-19 19:54:17 +0300

    farisupdate

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
@@ -15,9 +17,60 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int liftDiceNumber = 1;
+  int rightDiceNumber = 1;
+  void changeDiceFace(){
+    setState(() {
+      liftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              child: Image.asset('images/dice$liftDiceNumber.png'),
+              style: TextButton.styleFrom(
+                padding: EdgeInsets.all(16.0),
+              ),
+              onPressed: () {
+                changeDiceFace();
+                },
+            ),
+          ),
+          Expanded(
+            child: TextButton(child: Image.asset('images/dice$rightDiceNumber.png'),
+              style: TextButton.styleFrom(
+                padding: EdgeInsets.all(16.0),
+
+              ),
+              onPressed: () {
+                changeDiceFace();
+              },
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
+
+// int generateRandomNumber(int num){
+//   return Random().nextInt(6) + 1;
+//
+// }
+
+
+
